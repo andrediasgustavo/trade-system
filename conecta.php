69a81992745d8343edadec7f48c260d327466ff3
André Dias <andrediasgustavo@gmail.com>
2016-06-20 00:43:25 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -1,4 +1,4 @@
 <?php
-#$conexao = mysqli_connect('mysql.hostinger.com.br', 'u311552594_loja', '', 'u311552594_loja');
+$conexao = mysqli_connect('mysql.hostinger.com.br', 'u826440235_trade', 'e3a9m9310389', 'u826440235_trade');
 
-$conexao = mysqli_connect('localhost', 'root', 'e3a9m9310389', 'acoes');
+#$conexao = mysqli_connect('localhost', 'root', '', 'acoes');
